9ca8816bd65027fb279820c9731877d9fbf552b5
Stijn Tintel <stijn@linux-ipv6.be>
2024-01-31 17:59:37 +0200

    routes: add /v1/models
    This will return all the models configured in the TOML config file,
    which is needed for compatibility with some OpenAI clients that expect
    this API endpoint (e.g. Willow Auto Correct).

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,5 +2,7 @@ pub(crate) mod chat;
 pub(crate) mod completions;
 
 mod health_check;
+mod models;
 
 pub(crate) use health_check::health_check;
+pub(crate) use models::get;
---
+++ src/routes/models.rs
@@ -0,0 +1,35 @@
+use axum::extract::State;
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use openai_dive::v1::resources::model::{ListModelResponse, Model};
+use tracing::instrument;
+
+use crate::startup::AppState;
+
+#[instrument(name = "routes::models::get", skip(state))]
+pub async fn get(State(state): State<AppState>) -> Response {
+    let mut model_names: Vec<String> = Vec::new();
+
+    for model_type in state.config.models.keys() {
+        for model in state.config.models.get(model_type).unwrap().keys() {
+            model_names.push(model.clone());
+        }
+    }
+
+    let mut response: ListModelResponse = ListModelResponse {
+        data: Vec::new(),
+        object: String::from("list"),
+    };
+
+    for model_name in model_names {
+        let model = Model {
+            id: model_name,
+            created: 1_700_000_000,
+            object: String::from("model"),
+            owned_by: String::from("original owners"),
+        };
+        response.data.push(model);
+    }
+
+    Json(response).into_response()
+}
