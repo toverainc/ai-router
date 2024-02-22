36fcf8edd9a5ef8530af57cd810495a2d877a4fc
Stijn Tintel <stijn@linux-ipv6.be>
2024-02-22 13:18:47 +0200

    errors: add OpenAI error types
    Ideally these go into the openai_dive crate, but let's keep them here
    until we consider it ready.

--- src/errors.rs
+++ src/errors.rs
@@ -2,6 +2,7 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 pub struct AppError(anyhow::Error);
@@ -23,4 +24,31 @@ where
     fn from(err: E) -> Self {
         Self(err.into())
     }
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum OpenAIErrorCode {
+    InvalidApiKey,
+    ModelNotFound,
+    UnknownUrl,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum OpenAIErrorType {
+    InvalidRequestError,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct OpenAIError {
+    pub error: OpenAIErrorData,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct OpenAIErrorData {
+    pub code: Option<OpenAIErrorCode>,
+    pub message: String,
+    pub param: Option<String>,
+    pub r#type: OpenAIErrorType,
 }
