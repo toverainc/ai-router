218d7cb821faf9963fac49065f9ea0dd274546f7
Stijn Tintel <stijn@linux-ipv6.be>
2024-03-31 13:15:08 +0300

    state: add model to prompt hashmap
    Convert the prompt format to model map into a hashmap with model names
    as keys and prompt format as value for fast lookup.

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{
     backend::{Backend, Backends},
     config::AiRouterConfigFile,
@@ -15,6 +17,7 @@ pub enum BackendTypes<O, T> {
 pub struct State {
     pub backends: Backends,
     pub config: AiRouterConfigFile,
+    pub models_prompt: HashMap<String, String>,
     pub templater: Templater,
     pub tokenizers: Tokenizers,
 }
@@ -22,13 +25,23 @@ pub struct State {
 impl State {
     pub async fn new(config_file: &AiRouterConfigFile) -> Self {
         let backends = Backend::init(config_file).await;
+        let mut models_prompt = HashMap::new();
         let templater = Templater::new(&config_file.daemon.template_dir)
             .expect("failed to initialize templater");
         let tokenizers = Tokenizers::new(&config_file.models);
 
+        if let Some(prompt_models) = &config_file.prompt_models {
+            for (prompt_format, models) in prompt_models {
+                for model in &models.models {
+                    models_prompt.insert(String::from(model), String::from(prompt_format));
+                }
+            }
+        }
+
         Self {
             backends,
             config: config_file.clone(),
+            models_prompt,
             templater,
             tokenizers,
         }
