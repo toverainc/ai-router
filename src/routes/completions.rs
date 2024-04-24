821475dd8b9a7601b706094ac72de8dbfed966b1
Stijn Tintel <stijn@linux-ipv6.be>
2024-04-24 18:04:55 +0300

    routes/completions: fix clippy significant_drop_in_scrutinee warning

--- src/routes/completions.rs
+++ src/routes/completions.rs
@@ -16,15 +16,10 @@ use crate::state::{BackendTypes, State};
 pub async fn completion(
     AxumState(state): AxumState<Arc<State>>,
     mut request: Json<CompletionCreateParams>,
-) -> Response {
+) -> Result<Response, AiRouterError<String>> {
     if let Some(models) = state.config.models.get(&AiRouterModelType::ChatCompletions) {
         if let Some(model) = models.get(&request.model) {
-            let mut request_data = match AiRouterRequestData::build(model, &request.model, &state) {
-                Ok(d) => d,
-                Err(e) => {
-                    return e.into_response();
-                }
-            };
+            let mut request_data = AiRouterRequestData::build(model, &request.model, &state)?;
 
             if let Some(backend_model) = model.backend_model.clone() {
                 request.model = backend_model;
@@ -34,27 +29,28 @@ pub async fn completion(
 
             let Some(backend) = state.backends.get(model_backend) else {
                 tracing::warn!("backend: {:#?}", state.backends);
-                return AiRouterError::InternalServerError::<String>(format!(
+                return Err(AiRouterError::InternalServerError::<String>(format!(
                     "backend {model_backend} not found"
-                ))
-                .into_response();
+                )));
             };
 
             match &backend.client {
                 BackendTypes::OpenAI(_) => {
-                    return "legacy completions not supported in OpenAI backend".into_response();
+                    return Ok("legacy completions not supported in OpenAI backend".into_response());
                 }
                 BackendTypes::Triton(c) => {
-                    return triton_routes::completions::compat_completions(
+                    return Ok(triton_routes::completions::compat_completions(
                         c.clone(),
                         request,
                         &mut request_data,
                     )
-                    .await;
+                    .await);
                 }
             }
         }
     }
 
-    return AiRouterError::ModelNotFound::<String>(request.model.clone()).into_response();
+    Err(AiRouterError::ModelNotFound::<String>(
+        request.model.clone(),
+    ))
 }
